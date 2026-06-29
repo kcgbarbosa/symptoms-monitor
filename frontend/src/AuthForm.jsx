4fc4598a7c21d7aa721e4edc57c9c9f722fadb8d
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-06-29 15:04:58 -0400

    create handlesubmit handler

--- frontend/src/AuthForm.jsx
+++ frontend/src/AuthForm.jsx
@@ -3,6 +3,37 @@ import { supabase } from '../src/config/supabase.js';
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 
-function AuthForm() {}
+function Auth() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLogin, setIsLogin] = useState(true);
+  const navigate = useNavigate();
 
-export default AuthForm;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let data;
+    let error;
+    if (isLogin) {
+      ({ data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      }));
+    } else {
+      ({ data, error } = await supabase.auth.signUp({ email, password }));
+    }
+    if (error) {
+      console.log('Error with handleSubmit', error);
+      toast.error('Something went wrong');
+    } else {
+      navigate('/');
+    }
+  };
+
+  return (
+    <main>
+     
+    </main>
+  );
+}
+
+export default Auth;
