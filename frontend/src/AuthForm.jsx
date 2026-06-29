00e6885ecc63cee56411b8f51ee0ca75d14019e8
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-06-29 15:03:45 -0400

    feat: create authform

---
+++ frontend/src/AuthForm.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { supabase } from '../src/config/supabase.js';
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+
+function AuthForm() {}
+
+export default AuthForm;
