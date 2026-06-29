1a580bffc682d6c6696c499a64c4b359dc9b99e5
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-06-29 19:48:37 -0400

    feat: add checkSession and auth route handling

--- frontend/src/layouts/ProtectedRoute.jsx
+++ frontend/src/layouts/ProtectedRoute.jsx
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { supabase } from '../config/supabase';
 import { Navigate, Outlet } from 'react-router-dom';
 
 export default function ProtectedRoute() {
-  const [loading, setLoading] = useState(false);
-  const session = supabase.auth.getSession();
+  const [loading, setLoading] = useState(true);
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    async function checkSession() {
+      const response = await supabase.auth.getSession();
+      setSession(response.data.session);
+      setLoading(false);
+    }
+    checkSession();
+  }, []);
+
+  if (loading) return null;
   if (session) {
     return <Outlet />;
   } else {
