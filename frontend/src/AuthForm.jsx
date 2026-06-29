a7b243456526d28c478e3d1b63eb1a9915341538
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-06-29 15:13:02 -0400

    fix: change input type text to password

--- frontend/src/AuthForm.jsx
+++ frontend/src/AuthForm.jsx
@@ -23,7 +23,7 @@ function Auth() {
     }
     if (error) {
       console.log('Error with handleSubmit', error);
-      toast.error('Something went wrong');
+      toast.error(error.message);
     } else {
       navigate('/');
     }
@@ -42,7 +42,7 @@ function Auth() {
         />
         <label>Password</label>
         <input
-          type="text"
+          type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
