23bf404e7438589fe14f3fd106e1bb55167de1db
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-07-14 19:24:08 -0400

    style: implement AppHeader

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import AppSidebar from './AppSidebar';
-import ThemeToggle from '../components/ui/ThemeToggle';
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import AppHeader from './AppHeader';
+import { SidebarProvider } from '@/components/ui/sidebar';
 import { TooltipProvider } from '@/components/ui/tooltip';
 
 const Layout = () => {
@@ -9,13 +9,12 @@ const Layout = () => {
     <TooltipProvider>
       <SidebarProvider>
         <AppSidebar />
-        <ThemeToggle />
-        <main className="flex-1 min-w-0">
-          <SidebarTrigger className="m-2" />
-          <div className="max-w-7xl mx-auto p-6 lg:p-8">
+        <div className="flex min-w-0 flex-1 flex-col">
+          <AppHeader />
+          <main className="max-w-7xl w-full mx-auto p-6 lg:p-8">
             <Outlet />
-          </div>
-        </main>
+          </main>
+        </div>
       </SidebarProvider>
     </TooltipProvider>
   );
