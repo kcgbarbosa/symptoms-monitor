0d76c0ae1b94268e044a7cf3919a75133e7a84e7
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-07-13 20:48:02 -0400

    feat: change Route to RouteProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './layouts/Layout.jsx';
 import HomePage from './pages/HomePage.jsx';
 import EntriesPage from './pages/EntriesPage.jsx';
@@ -7,6 +7,24 @@ import AuthPage from './pages/AuthPage.jsx';
 import ProtectedRoute from './layouts/ProtectedRoute.jsx';
 import { useEffect } from 'react';
 import { useAuthStore } from '../store/useAuthStore.js';
+
+const router = createBrowserRouter([
+  { path: '/auth', element: <AuthPage /> },
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        element: <ProtectedRoute />,
+        children: [
+          { index: true, element: <HomePage /> },
+          { path: 'entries', element: <EntriesPage /> },
+          { path: 'trends', element: <TrendsPage /> },
+        ],
+      },
+    ],
+  },
+]);
 
 function App() {
   const { initAuth } = useAuthStore();
@@ -18,16 +36,7 @@ function App() {
   }, []);
   return (
     <div className="min-h-screen bg-gray-100">
-      <Routes>
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/" element={<Layout />}>
-          <Route element={<ProtectedRoute />}>
-            <Route index element={<HomePage />} />
-            <Route path="entries" element={<EntriesPage />} />
-            <Route path="trends" element={<TrendsPage />} />
-          </Route>
-        </Route>
-      </Routes>
+      <RouterProvider router={router} />
     </div>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,10 @@
-import "./index.css";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
+import './index.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>,
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
