29eb343e849dfb72ad0829887319d8b533267183
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-07-14 14:59:56 -0400

    feat: add dark mode

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import AppSidebar from './AppSidebar';
+import ThemeToggle from '../components/ui/ThemeToggle';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { TooltipProvider } from '@/components/ui/tooltip';
 
@@ -8,6 +9,7 @@ const Layout = () => {
     <TooltipProvider>
       <SidebarProvider>
         <AppSidebar />
+        <ThemeToggle />
         <main className="flex-1 min-w-0">
           <SidebarTrigger className="m-2" />
           <div className="max-w-7xl mx-auto p-6 lg:p-8">
